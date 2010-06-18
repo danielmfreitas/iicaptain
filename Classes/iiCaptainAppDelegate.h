b92c59808a6a2a257656986768a70d671f23011a
Daniel Freitas <daniel.mfreitas@gmail.com>
2010-06-18 00:17:42 -0400

    Project creation

---
+++ Classes/iiCaptainAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  iiCaptainAppDelegate.h
+//  iiCaptain
+//
+//  Created by Daniel Freitas on 10-06-18.
+//  Copyright TEKSystems 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iiCaptainAppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow *window;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iiCaptain
+//
+//  Created by Daniel Freitas on 10-06-18.
+//  Copyright TEKSystems 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"iiCaptainAppDelegate");
+	[pool release];
+	return retVal;
+}
